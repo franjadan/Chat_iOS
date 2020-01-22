b79175c7e03e1b391cbbcb0991a804e4ef7849ff
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-01-22 21:14:20 +0100

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by francisco.adan on 22/01/2020.
+//  Copyright © 2020 francisco.adan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
